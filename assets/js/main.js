296770452504b566fd15e8d4ceae8907fb5be6d3
MunkoBabudorzhiev <munko2387@gmail.com>
2022-10-15 19:41:32 +0800

    15.10.22 19:41

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,6 +8,15 @@ $(document).ready(function () {
     $('.nav__toggler .nav__icon, .nav__list, .header').removeClass('open');
   });
 	const videos = new Swiper('.video__block .swiper', {
+		slidesPerView: 1,
+		spaceBetween: 30,
+		// Responsive breakpoints
+		breakpoints: {
+			992: {
+				slidesPerView: 2,
+				spaceBetween: 40
+			}
+		},
 		navigation: {
 			nextEl: '.video__block .swiper-button-next',
 			prevEl: '.video__block .swiper-button-prev',
