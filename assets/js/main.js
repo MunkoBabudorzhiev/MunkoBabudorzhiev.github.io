4ee7f2a5341a0d2a96008b80a203d684c0ccb32e
MunkoBabudorzhiev <munko2387@gmail.com>
2022-10-16 18:16:34 +0800

    16.10.22 18:14

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,17 @@
-// const { default: Swiper } = require("swiper");
-
+const videos = new Swiper('.video__block .swiper', {
+	slidesPerView: 1,
+	spaceBetween: 30,
+	breakpoints: {
+		992: {
+			slidesPerView: 2,
+			spaceBetween: 40
+		}
+	},
+	navigation: {
+		nextEl: '.video__block .swiper-button-next',
+		prevEl: '.video__block .swiper-button-prev',
+	},
+});
 $(document).ready(function () {
   $('.nav__toggler').click(function () {
     $('.nav__toggler .nav__icon, .nav__list, .header').toggleClass('open');
@@ -7,18 +19,5 @@ $(document).ready(function () {
   $('.nav__link').click(function () {
     $('.nav__toggler .nav__icon, .nav__list, .header').removeClass('open');
   });
-	const videos = new Swiper('.video__block .swiper', {
-		slidesPerView: 1,
-		spaceBetween: 30,
-		breakpoints: {
-			992: {
-				slidesPerView: 2,
-				spaceBetween: 40
-			}
-		},
-		navigation: {
-			nextEl: '.video__block .swiper-button-next',
-			prevEl: '.video__block .swiper-button-prev',
-		},
-	});
+	
 });
